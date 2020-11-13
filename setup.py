6dee00beffffed6f0c7c07eb16d41149f3880dec
root <nathan@clicksminuteper.net>
2020-11-13 18:38:15 +0000

    prepare for move to pycharm

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='botty',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://clicksminuteper.net',
+    author='Minion3665',
+    author_email='nathan@clicksminuteper.net',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
